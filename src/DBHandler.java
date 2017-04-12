01444c6f3c45da69e65ca3c4db15b8b382a5ab2c
Rasmus Horntvedt <rasmus.horntvedt0004@stud.hkr.se>
2017-04-12 19:41:34 +0200

    Created the system class and added code to the dbhandler class

--- src/DBHandler.java
+++ src/DBHandler.java
@@ -2,4 +2,5 @@
  * Created by woojen on 2017-04-12.
  */
 public class DBHandler {
+
 }
---
+++ src/System.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Rasmus on 2017-04-12.
+ */
+public class System {
+    public void login(){
+
+    }
+    public void logout(){
+
+    }
+}
