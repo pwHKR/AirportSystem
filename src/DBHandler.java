d2d42b614bad74708f24e6f04601ce46f4bc1813
Rasmus Horntvedt <rasmus.horntvedt0004@stud.hkr.se>
2017-04-12 19:57:50 +0200

    Added the system class and added code to dbhandler

--- src/DBHandler.java
+++ src/DBHandler.java
@@ -1,6 +1,45 @@
-/**
- * Created by woojen on 2017-04-12.
- */
-public class DBHandler {
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Properties;
 
+public class DBHandler {
+    private final String dbName;
+    private final String user;
+    private final String password;
+    private final String connectionURL;
+
+    public DBHandler() {
+        Properties p = loadProperties();
+        dbName = p.getProperty("dbName");
+        user = p.getProperty("user");
+        password = p.getProperty("password");
+        connectionURL = "jdbc:mysql://46.101.88.25/" + dbName + "?user=" + user + "&password=" + password + "&useSSL=false";
+    }
+
+    private Properties loadProperties() {
+        Properties appProp = new Properties();
+        try(FileInputStream fis = new FileInputStream("app1.properties")) {
+            appProp.load(fis);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return  appProp;
+    }
+
+    public void printAll() {
+        try (Connection conn = DriverManager.getConnection(connectionURL)) {
+            Statement statement = conn.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM Airplane");
+
+            while(rs.next()) {
+                System.out.printf("Passenger capacity: [%d] Airplaine top speed --> %s%n", rs.getInt("passengerCapacity"),
+                        rs.getString("maxSpeed"));
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 }
