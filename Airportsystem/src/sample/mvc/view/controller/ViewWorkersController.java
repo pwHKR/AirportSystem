1e064c0362732e03ef012cc6b533532cb4245c6c
pwHKR <peter.westin0028@stud.hkr.se>
2017-04-22 17:36:27 +0200

    Added SwitchScene Class

--- Airportsystem/src/sample/mvc/view/controller/ViewWorkersController.java
+++ Airportsystem/src/sample/mvc/view/controller/ViewWorkersController.java
@@ -4,18 +4,13 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
-import javafx.stage.Stage;
 import sample.mvc.model.DBHandler;
 import sample.mvc.model.DataStorage;
+import sample.mvc.model.SwitchScene;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -23,6 +18,8 @@ import java.util.ResourceBundle;
  * Created by Stefan on 2017-04-22.
  */
 public class ViewWorkersController implements Initializable {
+
+    SwitchScene sw = new SwitchScene();
 
     private ObservableList<String> workerlist = FXCollections.observableArrayList();
     private ObservableList<String> infolist = FXCollections.observableArrayList();
@@ -60,17 +57,7 @@ public class ViewWorkersController implements Initializable {
     @FXML
     private void returnToAdmin(ActionEvent ae) {
 
-        Node node = (Node) ae.getSource();
-        Stage stage = (Stage) node.getScene().getWindow();
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("../Admin.fxml"));
-        Parent root = null;
-        try {
-            root = loader.load();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
+        sw.GoTo(ae, "Admin.fxml");
 
     }
 }
