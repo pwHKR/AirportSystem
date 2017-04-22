4ce2737dfd1bb0da04c9aea74e3335829fb0f24a
pwHKR <peter.westin0028@stud.hkr.se>
2017-04-22 17:32:14 +0200

    Added SwitchScene Class

---
+++ Airportsystem/src/sample/mvc/model/SwitchScene.java
@@ -0,0 +1,32 @@
+package sample.mvc.model;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * Created by woojen on 2017-04-22.
+ */
+public class SwitchScene {
+    public void GoTo(ActionEvent ae, String fxmlFile) {
+        Node node = (Node) ae.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/" + "" + fxmlFile + ""));
+        Parent root = null;
+        try {
+            root = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+
+    }
+
+
+}
